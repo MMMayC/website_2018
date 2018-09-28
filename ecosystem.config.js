ac64bef85ae9c2e1a9bbd2e91096b692c250337d
MMMayC <changingvan@gmail.com>
2018-09-29 08:38:37 +1000

    update deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'git@github.com:MMMayC/website_2018.git',
       path: '/home/ubuntu/website_2018',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      'post-deploy': 'npm install && npm run build && pm2 startOrRestart ecosystem.config.js'
     }
   }
 }
