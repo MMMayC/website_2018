1332188f0365a7ae17d819ac439445b605c4ce1b
MMMayC <changingvan@gmail.com>
2018-10-01 13:34:04 +1000

    Desktop design

--- src/components/home.js
+++ src/components/home.js
@@ -5,17 +5,17 @@ class Home extends React.Component {
 		return(
 			<div className='Home Grid-Item'>
 				<img src='media/jc_logo.svg' className='Home-Logo' />
-				<div className='Home-Copy'>
-					<h1 className='Home-Title'>Junwen Chen</h1>
-					<p className='Home-Subtitle'>Developer / Designer</p>
-					<p className='Home-Contact-Email'>
-						<a href='mailto:mayjunwenchen@gmail.com' className='Home-Contact-Link'> mayjunwenchen@gmail.com</a>
+				<div className='Home-Content'>
+					<h1 className='Home-Content-Title'>Junwen Chen</h1>
+					<p className='Home-Content-Subtitle'>Developer / Designer</p>
+					<p className='Home-Content-Email'>
+						<a href='mailto:mayjunwenchen@gmail.com' className='Home-Content-Link'> mayjunwenchen@gmail.com</a>
 					</p>
-					<p className='Home-Contact-Social'>
-						<a className='Home-Contact-Link' href='https://www.linkedin.com/in/junwenchen/' target='_blank'>
+					<p className='Home-Content-Social'>
+						<a className='Home-Content-Link' href='https://www.linkedin.com/in/junwenchen/' target='_blank'>
 							<i className='fab fa-linkedin'></i>
 						</a>
-						<a className='Home-Contact-Link' href='https://github.com/MMMayC' target='_blank'>
+						<a className='Home-Content-Link' href='https://github.com/MMMayC' target='_blank'>
 							<i className='fab fa-github-square'></i>
 						</a>
 					</p>
