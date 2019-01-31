578870faa9708ce9df9de555a4d93303dc45d206
Junwen Chen <mayjunwenchen@gmail.com>
2019-01-31 16:03:25 +1100

    .

--- src/components/about.js
+++ src/components/about.js
@@ -11,9 +11,9 @@ class About extends React.Component {
 					<img className='About-Content-Photo' src='/media/profile_2018_square.jpg' />
 					<div className='About-Content-Intro'>
 						<p>Hey, I'm Junwen. I'm a full-stack developer and UX designer.</p>
-						<p>At work, I'm a full-stack developer with a passion writing high-quality code. I have a wide range of skills (.NET / Javascript / Node.js / React / AWS, etc.) and can pick up new technologies quickly. And thanks to my experience in design, I always keep end-users in mind and have a keen eye for detail.</p>
-						<p>As a UX designer, I believe technology cannot make a difference if it doesn't solve a problem. I love finding problems in real life and coming up with solutions using Design Thinking methods. Then I implement the solutions with technologies. Check out my projects below.</p>
-						<p>My side husstle is to coach design students creative coding. I enjoy it because I like helping people to make their cool ideas happen. </p>
+						<p>I'm passionate about writing high-quality code. I have a wide range of skills (.NET / Javascript / Node.js / React / AWS, etc.) and can pick up new technologies quickly. And thanks to my experience in design, I always keep end-users in mind and have a keen eye for detail.</p>
+						<p>I believe technology cannot make a difference if it doesn't solve a problem. I love finding problems in real life, coming up with solutions using Design Thinking methods and then implementing the solutions with technologies. Check out my projects below.</p>
+						<p>My side hustle is to coach design students creative coding. I enjoy it because I like helping people to make their cool ideas happen. </p>
 					</div>
 				</div>
 			</div>
